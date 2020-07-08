33ae73aca2c8c6596fb1f6a1229c740c5c0235e7
lukmanulha-kim <61487253+lukmanulha-kim@users.noreply.github.com>
2020-07-08 20:01:28 +0700

    Update v_naik.php

--- application/views/siswa/v_naik.php
+++ application/views/siswa/v_naik.php
@@ -18,33 +18,13 @@
 			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<form action="" method="post">
-							<input type="text" name="i_kelasbaru" placeholder="Nama Kelas" class="form-control">
-							<table data-toggle="table"  data-show-refresh="true" data-show-toggle="true" data-show-columns="true" data-search="true" data-select-item-name="toolbar1" data-pagination="true" data-sort-name="name" data-sort-order="desc">
-							    <thead>
-								    <tr>
-								        <th data-field="no" data-sortable="true" > </th>
-								        <th data-field="nama" data-sortable="true">Nama Siswa</th>
-								        <th data-field="kelas"  data-sortable="true">Kelas</th>
-								        <th data-field="status" data-sortable="true">Status</th>
-								    </tr>
-							    </thead>
-							    <tbody>
-							    	<?php $no=1; foreach ($siswa->result() as $dataSiswa) { ?>
-							    	<tr>
-							    		<td><center><input type="checkbox" name="i_siswa[]" value=""></center></td>
-							    		<td><?php echo $dataSiswa->nama_siswa ?></td>
-										<td><?php echo $dataSiswa->kelas ?></td>
-										<td><?php echo $dataSiswa->status ?></td>
-							    	</tr>
-							    	<?php } ?>
-							    </tbody>
-							</table>
-						</form>
+						<div class="alert alert-danger">
+							Hubungi Developer Via E-Mail <code>lukmanha73@gmail.com</code> Untuk Versi Lengkap ☺
+						</div>
 					</div>
 				</div>
-			</div>
-		</div><!--/.row-->
+			</div><!-- /.col-->
+		</div><!-- /.row -->
 		
 		
 	</div><!--/.main-->
